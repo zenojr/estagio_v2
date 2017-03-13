f22956bba841f34cd8ddd9be28f7f4d1f8e43a86
Zeno França Junior <zfrancajunior@gmail.com>
2017-03-13 14:25:54 -0300

    refatorando
    refatorando

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-var express = require('express'); //OLD
-var app = express(); //OLD
+var express = require('express');
+var app = express();
 
-//var app = require('./config/express.js')();
-
-app.set('view engine', 'ejs'); //OLD
-
-app.get('/public', function(req,res){
-	console.log('carregou public');
-});
-
-app.use(express.static('public')); //verificar !!!!!
-
-app.get('/principal', function(req,res){
-	res.render('principal/login');
-});
-
-app.get('/inicial', function(req,res){
-	res.render('principal/principal');
+app.get('/ged', function(req,res){
+	res.send("<html><body><h1>Listagem de arquivos</h1></body></html>");
 });
 
 
-
-app.listen(3000,  function(){
-	console.log('v8 Running!');
+app.listen(3000, function(){
+	console.log("V8 Running!!!");	
 });
 
-//verificar a possibilidade de mudança do arquivo de configuração do express, estava dando problema ao setar a pasta public
--- config/express.js
+++
@@ -1,6 +0,0 @@
-var app = require('express')();
-app.set('view engine', 'ejs');
-
-module. exports = function() {
-    return app;
-}
