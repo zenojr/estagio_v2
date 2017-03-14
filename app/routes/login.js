05685bea5d73522e88f5b5e4219049edb23549b7
Zeno França Junior <zfrancajunior@gmail.com>
2017-03-14 08:10:38 -0300

    tela de login funcional
    tela de login funcional, ainda sem validar o user e senha de forma 100%
    correta

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,8 +1,18 @@
 
 module.exports = function(app) {
     app.get("/ged",function(req, res) {
-        res.render('ged/login');
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+        	host : 'localhost',
+        	user : 'root',
+        	password : 'pass@word1',
+        	database : 'GED'
+        });
+        connection.query('select * from usuario', function(err,results){
+        	res.render('ged/login', {lista:results});
+        });
     });
-
-    
 }
+
+
+
--- public/javascripts/valida_user.js
+++ public/javascripts/valida_user.js
@@ -1,9 +1,27 @@
 
+var rotaLogin = require('./app/routes/login')(app); //sempre lembrar de passar como retorno o app
 
-var campos = [
-	document.querySelector('#user'),
-	document.querySelector('#senha')	
-];
+valida = function(app){
+	event.preventDefault();
+	console.log('chamou!!');
+	var userIn = document.querySelector('#user').value;
+	var senhaIn = document.querySelector('#senha').value;
+	console.log(userIn);
+	console.log(senhaIn);
+}
 
-console.log(campos);
+
+for(var i=0; i<lista.length; i++){
+	var id = lista[i].id;
+	var nome = lista[i].nome;
+	var login = lista[i].login;
+	var senha = lista[i].senha;
+	var email = lista[i].email;
+
+	console.log(nome);
+	console.log(email);
+}
+
+
+
 
